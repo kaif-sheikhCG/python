a313630f7dc724c2d70eab96d2bee815e3c21a4d
kaif-sheikhCG <kaif.sheikh.cg@gmail.com>
2026-08-22 15:16:48 +0530

    variable assignment commited

---
+++ 01 python basics (variable assignment)/python assignement.py
@@ -0,0 +1,30 @@
+#Qustion-24
+name="kaif"
+age=17
+city="delhi"
+print(name,age,city)
+#Question-25
+student_name = "kaif"
+student_roll=67
+student_branch="Btech cse"
+print(student_name,student_roll,student_branch)
+#Question-26
+marks=50
+marks=49
+print(marks)
+#Question-27
+name,age,city = "kaif",17,"delhi"
+print(name,age,city)
+#Question-28
+x=y=z=0
+print(x,y,z)
+#Question-29
+student1 = "kai"
+student_name = "kaiff"
+student_branch= "B.Tech"
+print(student1,student_name,student_branch)
+#Question-30
+student_name="kaif"
+student_age=17
+student_age=18
+print(student_name,student_age)
