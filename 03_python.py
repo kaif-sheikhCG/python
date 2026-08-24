5ad28dbcd3cf37686e435cebf442c3c0d0bc73e0
kaif-sheikhCG <kaif.sheikh.cg@gmail.com>
2026-08-24 17:22:12 +0530

    python assignment 3 commited

---
+++ 03_python.py
@@ -0,0 +1,75 @@
+#24 question
+student_name = "kaif"
+student_age = 17
+student_height = 5.6
+is_student = True
+student_result = None
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(student_result))
+
+#25 question
+a = 50
+b = 50.0
+c = "50"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#26 question
+a = True
+b = "True"
+
+print(type(a))
+print(type(b))
+
+#27 question
+a = None
+b = "None"
+
+print(type(a))
+print(type(b))
+
+#28 question
+
+value = 100
+
+print(type(value))
+
+value = "Hello"
+
+print(type(value))
+
+#29 question
+product_name = "Laptop"
+product_quantity = 5
+product_price = 50000.50
+product_available = True
+product_discount = None
+
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(product_available))
+print(type(product_discount))
+
+#30 question
+a = 10
+b = 10.0
+c = "10"
+d = True
+e = "True"
+f = None
+g = "None"
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
