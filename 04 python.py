d452e153e974db97825c3059d65a186fcc4e7481
kaif-sheikhCG <kaif.sheikh.cg@gmail.com>
2026-08-25 17:25:45 +0530

    04 python assignment cmmited

---
+++ 04 python.py
@@ -0,0 +1,151 @@
+#question 1
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#question 2
+c=16
+d=2.0
+print(c+d)
+print(c-d)
+print(c*d)
+print(c/d)
+print(c//d)
+print(c%d)
+print(c**d)
+
+#question 3
+phy=80
+che=85
+math=90
+print("Total marks=" (phy+che+math))
+print("Average marks=" (phy+che+math)/3)
+
+#question 4
+milk_price=75
+milk_quantity=1000
+coke_price=60
+coke_quantity=600
+print("Total prce=" (milk_price*milk_quantity)+(coke_price*coke_quantity))
+
+#question 5
+x=18
+y=17
+print(x%2)
+print(y%2)
+
+#question 6
+a=7
+b=2
+c=-7
+d=-2
+print(a/b)
+print(a//b)
+print(c/d)
+print(c//d)
+
+#question 7
+a=-10
+b=-3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#question 8
+a=10
+b=5
+c=-5
+d=-10
+print(a-b)
+print(a-c)
+print(c-b)
+print(c-d)
+
+#question 9
+print(a//b)
+print(c//b)
+print(a//c)
+print(d//c)
+
+#question 10
+print(a%b)
+print(c%b)
+print(a%c)
+print(d%c)
+
+#question 11
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+
+#question 12
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+print(20 - 10 / 2)
+print((20 - 10) / 2)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+#question 13
+a=True
+b=False
+print(type(a+b))
+print(type(a-b))
+print(type(a*b))
+print(type(a/b))
+print(type(a//b))
+print(type(a%b))
+print(type(a**b))
+
+#question 14
+True + 5
+False + 5
+True * 10
+False * 10
+True - 5
+False - 5
+
+#question 15
+a="kaif"
+b="sheikh"
+print(a+b)
+
+#question 16
+print(a*9)
+print(a*9.5)
+
+#question 17
+print(a+b)
+print(a-b)
+print(a*8)
+print(a/b)
+
+#question 18
+a=None
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#question 19
+print(b/0)
+c="kai"
+d="she"
+print(c-d)
+print(a*b)
